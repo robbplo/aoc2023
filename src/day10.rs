c343c568f96cb125cfcf1f5533b99aa8b00c4aba
Robbin Ploeger <robbin.ploeger@bettyblocks.com>
2023-12-10 14:06:16 +0100

    day 10 part 2

--- src/day10.rs
+++ src/day10.rs
@@ -1,17 +1,17 @@
-use std::{collections::HashSet, vec};
+use std::{collections::HashSet, fmt::Display};
 
 pub fn solve() {
     let input = std::fs::read_to_string("input/day10.txt").unwrap();
     println!("# Day 10");
     println!("Part 1: {}", part1(&input));
-    //println!("Part 2: {}", part2(&input));
+    println!("Part 2: {}", part2(&input));
 }
 
 const START: char = 'S';
 
 type Position = (isize, isize);
 
-#[derive(Debug, Copy, Clone)]
+#[derive(Debug, Copy, Clone, PartialEq)]
 enum Direction {
     North,
     West,
@@ -34,6 +34,35 @@ impl Map {
         self.map
             .get(*y as usize)
             .and_then(|line| line.get(*x as usize))
+    }
+
+    fn replace_non_loop_chars(&mut self) {
+        let loop_positions: HashSet<Position> = self.get_loop_positions().into_iter().collect();
+
+        for y in 0..self.map.len() {
+            for x in 0..self.map[y].len() {
+                if !loop_positions.contains(&(x as isize, y as isize)) {
+                    self.map[y][x] = '.'
+                }
+            }
+        }
+    }
+
+    fn get_loop_positions(&self) -> Vec<Position> {
+        let mut direction = self.start_directions()[0];
+        let mut position = self.start;
+        let mut result = vec![];
+
+        loop {
+            let (next_pos, next_dir) = self.traverse(&position, &direction);
+            result.push(next_pos);
+            position = next_pos;
+            direction = next_dir;
+            if self.get(&next_pos) == Some(&START) {
+                break;
+            }
+        }
+        result
     }
 
     fn traverse(&self, pos: &Position, direction: &Direction) -> (Position, Direction) {
@@ -98,9 +127,23 @@ impl Map {
     }
 }
 
+impl Display for Map {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        for line in self.map.iter() {
+            let mut buf = String::new();
+            for char in line {
+                buf.push(*char)
+            }
+            writeln!(f, "{}", buf)?;
+        }
+        Ok(())
+    }
+}
+
 impl From<&str> for Map {
     fn from(input: &str) -> Self {
         let map = input
+            .trim()
             .lines()
             .map(|line| line.chars().collect::<Vec<char>>())
             .collect::<Vec<Vec<char>>>();
@@ -118,24 +161,36 @@ impl From<&str> for Map {
 }
 
 fn part1(input: &str) -> i32 {
-    let map = Map::from(input.trim());
-    let mut direction = map.start_directions()[0];
-    let mut position = map.start;
-    let mut steps = 0;
-
-    loop {
-        steps += 1;
-        let (next_pos, next_dir) = map.traverse(&position, &direction);
-        position = next_pos;
-        direction = next_dir;
-        if map.get(&next_pos) == Some(&START) {
-            return steps / 2;
-        }
-    }
+    let map = Map::from(input);
+    map.get_loop_positions().len() as i32 / 2
 }
 
 fn part2(input: &str) -> i32 {
-    0
+    let mut map = Map::from(input);
+    map.replace_non_loop_chars();
+    let mut count = 0;
+    let include_start = map.start_directions().contains(&Direction::North);
+    for row in map.map {
+        let mut in_loop = false;
+        for char in row {
+            match char {
+                '|' | 'J' | 'L' => in_loop = !in_loop,
+                'S' => {
+                    if include_start {
+                        in_loop = !in_loop
+                    }
+                }
+                '.' => {
+                    if in_loop {
+                        count += 1
+                    }
+                }
+                _ => (),
+            }
+        }
+    }
+
+    count
 }
 
 #[test]
@@ -148,5 +203,31 @@ SJ.L7
 LJ...
 ";
     assert_eq!(part1(input), 8);
-    //assert_eq!(part2(input), 2);
+
+    let input2 = "
+..........
+.S------7.
+.|F----7|.
+.||OOOO||.
+.||OOOO||.
+.|L-7F-J|.
+.|II||II|.
+.L--JL--J.
+..........
+";
+
+    let input3 = "
+FF7FSF7F7F7F7F7F---7
+L|LJ||||||||||||F--J
+FL-7LJLJ||||||LJL-77
+F--JF--7||LJLJIF7FJ-
+L---JF-JLJIIIIFJLJJ7
+|F|F-JF---7IIIL7L|7|
+|FFJF7L7F-JF7IIL---7
+7-L-JL7||F7|L7F-7F7|
+L.L7LFJ|||||FJL7||LJ
+L7JLJL-JLJLJL--JLJ.L
+";
+    assert_eq!(part2(input2), 4);
+    assert_eq!(part2(input3), 10);
 }
