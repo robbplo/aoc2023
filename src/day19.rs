24a4f010183a5fea830b33b522a7caffaa2d4d3a
Robbin Ploeger <robbin.ploeger@bettyblocks.com>
2023-12-19 18:23:34 +0100

    day 19 part 2

--- src/day19.rs
+++ src/day19.rs
@@ -1,4 +1,4 @@
-use std::collections::HashMap;
+use std::{collections::HashMap, ops::RangeInclusive, panic};
 
 pub fn solve() {
     let input = std::fs::read_to_string("input/day19.txt").unwrap();
@@ -93,17 +93,6 @@ struct Rule {
 
 impl From<&str> for Rule {
     fn from(value: &str) -> Self {
-        // 3 options
-
-        // "a>1716:R"
-        // "m<1801:hdj"
-        // "A"
-        // "hdj"
-
-        // "[key][operator][value]:[operation]"
-        // "[operation]
-
-        // "px{a<2006:qkq,m>2090:A,rfg}"
         let mut chars = value.chars();
         if !chars.clone().any(|c| c == '>' || c == '<') {
             return Rule {
@@ -113,7 +102,6 @@ impl From<&str> for Rule {
                 operation: Operation::from(value),
             };
         }
-
         let key = chars.next().unwrap();
         let operator = match chars.next().unwrap() {
             '>' => Operator::GreaterThan,
@@ -148,18 +136,76 @@ impl Part {
 
 impl From<&str> for Part {
     fn from(value: &str) -> Self {
-        //{x=787,m=2655,a=1222,s=2876}
         let mut parts = value
             .trim_start_matches('{')
             .trim_end_matches('}')
             .split(',')
             .map(|s| s[2..].to_string().parse::<usize>().unwrap());
-
         let x = parts.next().unwrap();
         let m = parts.next().unwrap();
         let a = parts.next().unwrap();
         let s = parts.next().unwrap();
         Part { x, m, a, s }
+    }
+}
+
+#[derive(Debug, Clone)]
+struct PartRange {
+    x: RangeInclusive<usize>,
+    m: RangeInclusive<usize>,
+    a: RangeInclusive<usize>,
+    s: RangeInclusive<usize>,
+}
+
+impl Default for PartRange {
+    fn default() -> Self {
+        PartRange {
+            x: 1..=4000,
+            m: 1..=4000,
+            a: 1..=4000,
+            s: 1..=4000,
+        }
+    }
+}
+
+impl PartRange {
+    fn get_mut(&mut self, key: char) -> &mut RangeInclusive<usize> {
+        match key {
+            'x' => &mut self.x,
+            'm' => &mut self.m,
+            'a' => &mut self.a,
+            's' => &mut self.s,
+            _ => panic!("EVERYONE PANIC!"),
+        }
+    }
+    fn get(&self, key: char) -> &RangeInclusive<usize> {
+        match key {
+            'x' => &self.x,
+            'm' => &self.m,
+            'a' => &self.a,
+            's' => &self.s,
+            _ => panic!("EVERYONE PANIC!"),
+        }
+    }
+    fn with_rule(&self, rule: &Rule) -> Option<(Self, Self)> {
+        let mut new = self.clone();
+        let mut inverse = self.clone();
+        match rule.operator {
+            Operator::GreaterThan => {
+                let new_key = new.get_mut(rule.key);
+                let inverse_key = inverse.get_mut(rule.key);
+                *new_key = rule.value + 1..=*self.get(rule.key).end();
+                *inverse_key = *self.get(rule.key).start()..=rule.value;
+            }
+            Operator::LessThan => {
+                let new_key = new.get_mut(rule.key);
+                let inverse_key = inverse.get_mut(rule.key);
+                *new_key = *self.get(rule.key).start()..=rule.value - 1;
+                *inverse_key = rule.value..=*self.get(rule.key).end();
+            }
+            Operator::None => return None,
+        }
+        Some((new, inverse))
     }
 }
 
@@ -183,7 +229,6 @@ fn part1(input: &str) -> usize {
     }
     let initial = map.get("in").unwrap();
     let mut sum = 0;
-
     for part in parts.iter() {
         let mut current = initial;
         loop {
@@ -206,8 +251,65 @@ fn part1(input: &str) -> usize {
     sum
 }
 
+fn do_part2(
+    map: &HashMap<&str, &Workflow>,
+    key: &str,
+    current: PartRange,
+    acc: &mut Vec<PartRange>,
+) {
+    let mut kaastaart = current;
+    if let Some(workflow) = map.get(key) {
+        for rule in &workflow.rules {
+            match kaastaart.with_rule(&rule) {
+                Some((new, inverse)) => {
+                    match &rule.operation {
+                        Operation::Accepted => {
+                            acc.push(new);
+                        }
+                        Operation::Rejected => (),
+                        Operation::ProcessWith(name) => {
+                            do_part2(map, name, new, acc);
+                        }
+                    }
+                    kaastaart = inverse;
+                }
+
+                None => match &rule.operation {
+                    Operation::Accepted => {
+                        acc.push(kaastaart.clone());
+                    }
+                    Operation::Rejected => (),
+                    Operation::ProcessWith(name) => {
+                        do_part2(map, name, kaastaart.clone(), acc);
+                    }
+                },
+            }
+        }
+    }
+}
 fn part2(input: &str) -> usize {
-    0
+    let mut strparts = input.trim().split("\n\n");
+    let mut map: HashMap<&str, &Workflow> = HashMap::new();
+    let workflows = strparts
+        .next()
+        .unwrap()
+        .lines()
+        .map(Workflow::from)
+        .collect::<Vec<_>>();
+    for workflow in workflows.iter() {
+        map.insert(&workflow.name, &workflow);
+    }
+    let mut ranges = Vec::new();
+    do_part2(&map, "in", PartRange::default(), &mut ranges);
+    let mut sum = 0;
+    for range in ranges {
+        let x = range.x.size_hint().1.unwrap();
+        let m = range.m.size_hint().1.unwrap();
+        let a = range.a.size_hint().1.unwrap();
+        let s = range.s.size_hint().1.unwrap();
+        sum += x * m * a * s;
+    }
+    sum
 }
 
 #[test]
@@ -232,5 +334,5 @@ hdj{m>838:A,pv}
 {x=2127,m=1623,a=2188,s=1013}
 ";
     assert_eq!(part1(input), 19114);
-    assert_eq!(part2(input), 0);
+    assert_eq!(part2(input), 167409079868000);
 }
