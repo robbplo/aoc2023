095076aeff8598e2a930a2ac266ba7342b7e0036
Robbin Ploeger <robbin.ploeger@bettyblocks.com>
2023-12-19 09:56:44 +0100

    day 18 part 2

--- src/day18.rs
+++ src/day18.rs
@@ -21,6 +21,23 @@ impl Rule {
             _ => panic!("Invalid direction"),
         }
     }
+
+    fn offset_2(&self) -> (isize, isize) {
+        let dir = self.color.chars().last().unwrap();
+
+        match dir {
+            '0' => (1, 0),
+            '1' => (0, 1),
+            '2' => (-1, 0),
+            '3' => (0, -1),
+            _ => panic!("Invalid direction"),
+        }
+    }
+
+    fn bigcount(&self) -> isize {
+        let hex = self.color.chars().take(5).collect::<String>();
+        isize::from_str_radix(&hex, 16).unwrap()
+    }
 }
 
 impl From<&str> for Rule {
@@ -28,7 +45,13 @@ impl From<&str> for Rule {
         let mut parts = s.split_whitespace();
         let dir = parts.next().unwrap().chars().next().unwrap();
         let count = parts.next().unwrap().parse::<isize>().unwrap();
-        let color = parts.next().unwrap().to_string();
+        let color = parts
+            .next()
+            .unwrap()
+            .to_string()
+            .replace("#", "")
+            .replace("(", "")
+            .replace(")", "");
         Rule { dir, count, color }
     }
 }
@@ -51,7 +74,18 @@ fn shoelace(points: &[(isize, isize)]) -> isize {
 
 fn interior_points(points: &[(isize, isize)]) -> isize {
     let area = shoelace(&points);
-    ((points.len() as isize / 2) - 1 - area).abs()
+    let mut perimeter = 0;
+    for i in 0..points.len() - 1 {
+        let (x1, y1) = points[i];
+        let (x2, y2) = points[i + 1];
+
+        perimeter += (x2 - x1).abs() + (y2 - y1).abs();
+    }
+    let (x1, y1) = points.first().unwrap();
+    let (x2, y2) = points.last().unwrap();
+    perimeter += (x2 - x1).abs() + (y2 - y1).abs();
+
+    ((perimeter as isize / 2) - 1 - area).abs() + perimeter as isize
 }
 
 fn print_points(points: &[(isize, isize)]) {
@@ -76,18 +110,27 @@ fn part1(input: &str) -> usize {
     let (mut x, mut y) = (0, 0);
     for rule in rules {
         let (dx, dy) = rule.offset();
-        for _ in 0..rule.count {
-            x += dx;
-            y += dy;
-            points.push((x, y));
-        }
+        x += dx * rule.count;
+        y += dy * rule.count;
+        points.push((x, y));
     }
 
-    interior_points(&points) as usize + points.len()
+    interior_points(&points) as usize
 }
 
 fn part2(input: &str) -> usize {
-    0
+    let rules = input.trim().lines().map(Rule::from).collect::<Vec<_>>();
+    let mut points: Vec<(isize, isize)> = vec![];
+    let (mut x, mut y) = (0, 0);
+    for rule in rules {
+        let (dx, dy) = rule.offset_2();
+        let count = rule.bigcount();
+        x += dx * count;
+        y += dy * count;
+        points.push((x, y));
+    }
+
+    interior_points(&points) as usize
 }
 
 #[test]
